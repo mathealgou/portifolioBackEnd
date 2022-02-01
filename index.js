c1d26581fc9192d033a2bb740ac6f6cfaadcf817
Matheus <mathealgou@gmail.com>
2022-01-31 21:04:36 -0300

    Boilerplate ready

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const port = process.env.PORT || 3000;
 
 // Create a new express application instance
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
